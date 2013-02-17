974ef2ebc6a2aa82900e4571326ea1a15e0b5ffa
cbrem <connorbrem@gmail.com>
2013-02-17 13:39:39 -0500

    the same hunt cannot be created multiple times

--- app.js
+++ app.js
@@ -54,11 +54,8 @@ app.get("/foo", function(request, response){
 // for JOIN request to hunts, tell client if hunt exists
 app.get("/hunts/:hunt", function (request, response) {
   var hunt = request.params.hunt;
-  var exists;
-  if (hunt in globalHuntData) exists = true;
-  else exists = false;
   response.send({
-    "exists": exists
+    "exists": (exists in globalHuntData)
   });
 });
 
@@ -80,6 +77,13 @@ app.get("/:hunt/admin", function (request, response) {
 app.post("/:hunt", function (request, response) {
   console.log("POSTING new hunt!");
   var hunt = request.params.hunt;
+
+  //check if hunt already exists
+  if (hunt in globalHuntData) response.send({
+    "error" : false,
+    "alreadyExists": true
+  });
+
   // ** may change this later to create object AFTER first save **
   // create new empty hunt object with the creator's inputted hunt name
   huntObj = {};
@@ -98,12 +102,14 @@ app.post("/:hunt", function (request, response) {
     if (err) {
       console.log("Error thrown: " + err);
       response.send({
-        "error": true
+        "error": true,
+        "alreadyExists": false
       })
     }
     else {
       response.send({
-        "error": false
+        "error": false,
+        "alreadyExists": false
       });
     }
   });
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -39,8 +39,10 @@ $(document).ready(function () {
       url: "/" + urlHuntName,
       data: {"newHuntName": newHuntName},
       success: function(data) {
-        // navigate to the new admin webpage
-        if (!data.error) {
+        if (data.alreadyExists) {
+          console.log("A hunt named '" + newHuntName + "'already exists!");
+        } else if (!data.error) {
+          // navigate to the new admin webpage
           console.log("Successfuly created new hunt");
           window.location = "./" + urlHuntName + "/admin";
         }
