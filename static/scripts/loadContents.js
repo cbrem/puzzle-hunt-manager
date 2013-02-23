306cbd2aac3c378cad3d4d1dcfad6197c94846a6
Leon Zhang <lwzhang@andrew.cmu.edu>
2013-02-23 14:34:30 -0500

    extended content loader to load user data (except for clues)

--- static/scripts/loadContents.js
+++ static/scripts/loadContents.js
@@ -1,30 +1,60 @@
+function fillEach(selector, textContent){
+    $(selector).each(function(i, elem){
+        $(elem).text(textContent);
+    });
+}
+
 // dis is loading up the admin view for a hunt
 $(document).ready(function(){
   // make ajax call to app.js to get the admin data for the given hunt,
   // then populate the interface with the data entries
   var currentUrl = window.location.pathname;
   var urlList = currentUrl.split("/");
-  assert(urlList[1] === "hunts");
-  var urlHuntName = urlList[2];
+  var huntBaseIndex = urlList.indexOf("hunts");
+  assert(huntBaseIndex !== -1);
+  var urlHuntName = urlList[huntBaseIndex+1];
+  var urlUserName = undefined;
+  
+  if(urlList.length > huntBaseIndex+2){
+      urlUserName = urlList[huntBaseIndex+2];
+  }
 
   // this loads the information from the specific hunt's data to the page
-  function loadPageLabels(huntData) {
+  function loadPageInfo(huntData) {
   	// title <- raw hunt name
-  	$(".hunt-name").each(function(i, elem){
-        $(elem).html(huntData["rawname"]);
-    });
+    fillEach(".hunt-name", huntData.rawname);
     
   	// _ total clues <- number of clues
-  	var numClues = huntData["clues"].length;
-  	$(".num-total-clues").each(function(id, elem){
-        $(elem).html(numClues);
-    });
+  	var numClues = huntData.clues.length;
+    fillEach(".num-total-clues", numClues);
     
   	// _ total users <- number of users
-  	var numUsers = getObjectSize(huntData["users"]);
-  	$(".num-total-teams").each(function(id, elem){
-        $(elem).html(numUsers);
-    });
+  	var numUsers = getObjectSize(huntData.users);
+    fillEach(".num-total-teams", numUsers);
+    
+    /* load user data, if present */
+    if(urlUserName !== undefined && urlUserName in huntData.users){
+        var userData = huntData.users[urlUserName];
+        var userName = userData.username;
+        
+        fillEach(".team-name", userName);
+        
+        var numSolvedClues = userData.progress.length;
+        fillEach(".num-solved-clues", numSolvedClues);
+        
+        if(numSolvedClues < numClues){
+            var currentClueNum = numSolvedClues+1;
+            fillEach(".curr-clue-num", currentClueNum);
+        }
+        
+        // if there is a canvas available on the page
+        if($("#map-canvas").length !== 0 && 
+           $("#canvas-wrapper").length !== 0 &&
+           loadCanvasMap !== undefined)
+        {
+            loadCanvasMap(numSolvedClues, numClues);
+        }
+    }
   }
 
   function loadClues(huntData) {
@@ -40,7 +70,7 @@ $(document).ready(function(){
     url: "/info/" + encodeURIComponent(urlHuntName),
     success: function(data) {
       if (data.exists) {
-        loadPageLabels(data.hunt);
+        loadPageInfo(data.hunt);
         //fillScoreboard(data.hunt);
       } else {
         console.log("Something's real messed up with getting data to load this.")
