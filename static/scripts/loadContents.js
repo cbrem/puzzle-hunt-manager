72614546b7056c7f6ef2b91d80ce80e8b0c09ded
Erik Pintar <elpintar@gmail.com>
2013-02-23 14:41:35 -0500

    added loadClues

--- static/scripts/loadContents.js
+++ static/scripts/loadContents.js
@@ -1,11 +1,12 @@
 // dis is loading up the admin view for a hunt
 $(document).ready(function(){
-  // make ajax call to app.js to get the admin data for the given hunt,
+  // todo: make ajax call to app.js to get the admin data for the given hunt,
   // then populate the interface with the data entries
   var currentUrl = window.location.pathname;
   var urlList = currentUrl.split("/");
   assert(urlList[1] === "hunts");
   var urlHuntName = urlList[2];
+  var userName = urlList[3];
 
   // this loads the information from the specific hunt's data to the page
   function loadAdminPage(huntData) {
@@ -20,7 +21,18 @@ $(document).ready(function(){
   }
 
   function loadClues(huntData) {
-  	
+  	var clues = huntData["clues"];
+  	var numClues = clues.length;
+  	var clueTable = $("#clues");
+  	for (var i = 0; i < numClues; i++) {
+  		var clue = clues[i];
+  		var newClueRow = $("<tr>").attr("class", "clue-row");
+  		var clueNum = $("<td>").attr("class", "clue-num").html(i);
+  		var clueDesc = $("<td>").attr("class", "clue-desc").html(clue["desc"]);
+  		var clueAns = $("<td>").attr("class", "clue-ans").html(clue["ans"]);
+  		newClueRow.append(clueNum).append(clueDesc).append(clueAns);
+  		clueTable.append(newClueRow);
+  	};
   }
 
   function fillScoreboard(huntData) {
@@ -32,14 +44,19 @@ $(document).ready(function(){
     url: "/info/" + urlHuntName,
     success: function(data) {
       if (data.exists) {
-        loadAdminPage(data.hunt);
-        //fillScoreboard(data.hunt);
+      	// for admin!
+      	if (userName === "admin") {
+	        loadAdminPage(data.hunt);
+	        //fillScoreboard(data.hunt);
+	        loadClues(data.hunt);
+	      }
+	      // for all other users
+	      else {
+
+	      }
       } else {
         console.log("Something's real messed up with getting data to load this.")
       }
-    },
-    error: function(data){
-        console.log("error", data);
     }
   });
 
